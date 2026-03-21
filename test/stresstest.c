e6ce84d991256670486a0fa57cb29eebf84606e0
The-codeMachine <coolestbro5113@gmail.com>
2026-03-21 08:07:26 -0600

    Stress test

---
+++ test/stresstest.c
@@ -0,0 +1,42 @@
+#include "../src/include/matrix.h"
+#include "../src/include/multithreading.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RANDOM_MIN 1.0
+#define RANDOM_MAX 10.0
+
+void fill_random(Matrix *a) {
+    for (size_t i = 0; i < a->rows; ++i)
+    {
+        for (size_t j = 0; j < a->cols; ++j)
+        {
+
+            double r = (double)rand() / RAND_MAX;
+            double value = RANDOM_MIN + r * (RANDOM_MAX - RANDOM_MIN);
+
+            matrix_set(a, i, j, value);
+        }
+    }
+}
+
+int main() {
+    srand(time(NULL));
+
+    Matrix* a = matrix_create(12000, 12000, 0);
+    Matrix* b = matrix_create(12000, 12000, 0);
+    Matrix* c = matrix_create(12000, 12000, 0);
+
+    fill_random(a);
+    fill_random(b);
+
+    matrix_multiply(a, b, c, BACKEND_GPU, -1);
+
+    matrix_free(a);
+    matrix_free(b);
+    matrix_free(c);
+
+    return 0;
+}
